850210037bf38d71cf19d00c96fbc878825796b9
smallxy-macos <2569224983@qq.com>
2025-07-19 18:18:17 +0800

    update 不用了封存

--- src/main/java/cn/cyanbukkit/cyanmenu/cyanlib/launcher/JavaHandle.java
+++ src/main/java/cn/cyanbukkit/cyanmenu/cyanlib/launcher/JavaHandle.java
@@ -1,28 +1,24 @@
 package cn.cyanbukkit.cyanmenu.cyanlib.launcher;
 
-
 import cn.cyanbukkit.cyanmenu.cyanlib.loader.KotlinBootstrap;
 
 public class JavaHandle {
 
     public static void getJavaVersionToDownloadHikaricp() {
-        String javaVersion = System.getProperty("java.version");
-        String[] split = javaVersion.split("\\.");
-        int version = Integer.parseInt(split[1]);
-//        if (version >= 11) {
-            KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","5.1.0");
-            // 下载 slf4j
-        KotlinBootstrap.loadDepend("org.slf4j", "slf4j-api","2.1.0-alpha1");
-//        } else if (version == 8) {
-//            KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","4.0.3");
-//        } else if (version == 7) {
-//            KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","2.4.13");
-//        } else if (version == 6) {
-//            KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","2.3.13");
-//        } else {
-//            System.out.println("Java " + version);
-//        }
+        double classVersion = Double.parseDouble(System.getProperty("java.class.version"));
+        System.out.println("classVersion: " + classVersion);
+        // 17 = 61.0
+        // 11 = 55.0
+        // 8 = 52.0
+        if (classVersion >= 55.0) {
+            KotlinBootstrap.loadDepend("com.zaxxer", "HikariCP", "5.1.0");
+        } else if (classVersion < 55.0 && classVersion >= 52.0) {
+            KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","4.0.3");
+        } else  {
+            KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","2.4.13");
+        }
         KotlinBootstrap.loadDepend("mysql","mysql-connector-java","8.0.25");
+        KotlinBootstrap.loadDepend("org.slf4j","slf4j-api","1.7.30");
     }
 
 }
